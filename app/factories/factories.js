2dd9e8bfbee8d2baef333e6a3cf550710267d65f
Bene <admin@benesmacbookpro.speedport_w_921v_1_39_000>
2015-08-31 20:58:25 +0200

    formatting

--- app/factories/factories.js
+++ app/factories/factories.js
@@ -1,7 +1,7 @@
 var app = angular.module('slApp', []);
 
 app.factory('factory1', function(){
-	var Aufgaben = [A,B,C,D,E,F];
+	var Aufgabe = "Aufgabe1";
 	var factory = {};
 	factory.getAufgaben =  function(){
 		return Aufgabe;
