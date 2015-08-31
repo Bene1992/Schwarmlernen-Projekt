efb793dbd95473348e1eb9183a2fab6c581f4841
Bene <admin@benesmacbookpro.speedport_w_921v_1_39_000>
2015-08-31 20:39:46 +0200

    Changes var app

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,3 +1,5 @@
+var app = angular.module('slApp', []);
+
 app.controller('easyController', function ($scope, factory1) {
 	$scope.Aufgabe = "Hallo"; //factory1.getAufgaben();
-}
+});
--- app/factories/factories.js
+++ app/factories/factories.js
@@ -1,4 +1,6 @@
-slApp.factory('factory1', function(){
+var app = angular.module('slApp', []);
+
+app.factory('factory1', function(){
 	var Aufgaben = [A,B,C,D,E,F];
 	var factory = {};
 	factory.getAufgaben =  function(){
