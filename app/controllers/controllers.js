eb21d1e0d3baf2cda52ad14519fc2b1170f99cf1
Bene <admin@benesmacbookpro.speedport_w_921v_1_39_000>
2015-09-02 22:08:14 +0200

    Scheiß jsTree

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,27 +1,52 @@
 var app = angular.module('slApp');
 
-var refDegree='';
-var childrenTargets = [];
+
+
 
 app.controller('easyController',['$scope','$http','Api', function ($scope,$http, Api) {
 
+
+	//https://www.jstree.com
+	// 6 create an instance when the DOM is ready
+    $('#jstree').jstree();
+    // 7 bind to events triggered on the tree
+    $('#jstree').on("changed.jstree", function (e, data) {
+      console.log(data.selected);
+	});
+	
+	$('#jstree').jstree(
+    	{
+			"core" : {
+                "check_callback": true,
+                },
+            "plugins" : ["search", "sort"]
+    	});
+	
+	
+	console.log($('#jstree').jstree("create_node",[ '#', "Haalo"]));
+	console.log($('#jstree').jstree().create_node('#', "Hallo", 'last'));
+	console.log($('#jstree').jstree('create_node', '#', { 'attr' : { 'id' : '1' } , 'text' : "Bene"}, 'last'));
+	console.log($('#jstree').jstree().create_node('#' , {'text':'new node', 'type':'valid_child'}));
+	
 	
 	
 	Api.getAllDegrees()
 		.then (function(data) {
-			$scope.Degree = data[0].properties.name;
-			Api.getDegreeBy(data[0].properties.uuid)
+			jQuery.each(data, function() {
+				console.log(this.properties.uuid);
+       			$scope.Degree = this.properties.name;
+				$('#jstree').jstree("create_node", '#', this.properties.name);
+				Api.getTargetsByRef(this.ref+'/targets')
 				.then (function(data2) {
-					refDegree = data2.ref;
-					$scope.Ref = data2.ref; 
-				}).then (function() {
-					Api.getTargetsByRef(refDegree+'/targets')
-					.then (function(data3) {
-						console.log(data3);
-						$scope.Targets=data3;
-					})
+					jQuery.each(data2, function() {
+       					
+       					
+   					})
 				})
 			})
+		})						
+   		
+		
 	
 	
 		
