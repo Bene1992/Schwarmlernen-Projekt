77f85f172ca773120c51af217e19de32ed3d7f9f
Bene <admin@benesmacbookpro.speedport_w_921v_1_39_000>
2015-09-03 01:51:36 +0200

    Startseite

--- app/app.js
+++ app/app.js
@@ -3,11 +3,16 @@ var app = angular.module('slApp', ['ngRoute']);
 //This configures the routes and associates each route with a view and a controller
 app.config(['$routeProvider',function ($routeProvider) {
     $routeProvider
-        .when('/',
+        .when('/Startseite',
+            {
+                controller: '',
+                templateUrl: 'app/partials/Startseite.html'
+            })
+        .when('/Lernziele',
             {
                 controller: 'easyController',
-                templateUrl: 'app/partials/view1.html'
+                templateUrl: 'app/partials/Lernziele.html'
             })
        
-        .otherwise({ redirectTo: 'app/partials/view1.html' });
+        .otherwise({ redirectTo: 'app/partials/Startseite.html' });
 }]);
