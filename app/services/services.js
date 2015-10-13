1d806c11aac43f53d609f28005ef1cbab881a711
Bene <admin@Benes-Mac-Book-Pro.local>
2015-10-13 14:30:37 +0200

    URL geändert

--- app/services/services.js
+++ app/services/services.js
@@ -17,103 +17,104 @@ app.factory('httpRequestInterceptor',['$cookies', function ($cookies) {
 
 
 app.service('Api', ['$http','$cookies', function($http,$cookies) {
-
+	
+	var url = 'http://ns319046.ip-37-59-6.eu/schwarmlernenapi'
 
 	this.getAllTargets=  function(){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/targets')
+		return $http.get(url+'/targets')
 	}
 	
 	this.getTarget= function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/targets/'+uuid)
+		return $http.get(url+'/targets/'+uuid)
 	}
 	
 	this.getTask= function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/tasks/'+uuid)
+		return $http.get(url+'/tasks/'+uuid)
 	}
 	
 	this.getInfo= function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/infos/'+uuid)
+		return $http.get(url+'/v1/infos/'+uuid)
 	}
 	
 	this.getSolution= function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/solutions/'+uuid)
+		return $http.get(url+'/solutions/'+uuid)
 	}
 	
 	this.saveTask= function(uuid,msg){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/tasks/'+uuid,msg)
+		return $http.put(url+'/tasks/'+uuid,msg)
 	}
 	
 	this.saveInfo= function(uuid,msg){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/infos/'+uuid,msg)
+		return $http.put(url+'/infos/'+uuid,msg)
 	}
 	
 	this.saveSolution= function(uuid,msg){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/solutions/'+uuid,msg)
+		return $http.put('url+/solutions/'+uuid,msg)
 	}
 	
 	this.submitTask= function(uuid){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/tasks/'+uuid+'/submit')
+		return $http.put(url+'/tasks/'+uuid+'/submit')
 	}
 	
 	this.submitInfo= function(uuid){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/infos/'+uuid+'/submit')
+		return $http.put(url+'/infos/'+uuid+'/submit')
 	}
 	
 	this.submitSolution= function(uuid){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/solutions/'+uuid+'/submit')
+		return $http.put(url+'/solutions/'+uuid+'/submit')
 	}
 	
 	
 	this.getRatingOfTask=  function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/tasks/'+uuid+'/ratings')
+		return $http.get(url+'/tasks/'+uuid+'/ratings')
 	}
 	
 	this.getMyRatingOfTask=  function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/tasks/'+uuid+'/rating')
+		return $http.get(url+'/tasks/'+uuid+'/rating')
 	}
 	
 	this.postRatingOfTask=  function(msg,uuid){
-		return $http.post('http://maximumstock.net/schwarmlernen/api/v1/tasks/'+uuid+'/ratings',msg)
+		return $http.post(url+'/tasks/'+uuid+'/ratings',msg)
 	}
 	
 	this.getRatingOfSolution=  function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/solutions/'+uuid+'/ratings')
+		return $http.get(url+'/solutions/'+uuid+'/ratings')
 	}
 	
 	this.getMyRatingOfSolution=  function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/solutions/'+uuid+'/rating')
+		return $http.get(url+'/solutions/'+uuid+'/rating')
 	}
 	
 	this.postRatingOfSolution=  function(msg,uuid){
-		return $http.post('http://maximumstock.net/schwarmlernen/api/v1/solutions/'+uuid+'/ratings',msg)
+		return $http.post(url+'/solutions/'+uuid+'/ratings',msg)
 	}
 	
 	this.getRatingOfInfo=  function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/infos/'+uuid+'/ratings')
+		return $http.get(url+'/infos/'+uuid+'/ratings')
 	}
 	
 	this.getMyRatingOfInfo=  function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/infos/'+uuid+'/rating')
+		return $http.get(url+'/infos/'+uuid+'/rating')
 	}
 	
 	this.postRatingOfInfo=  function(msg,uuid){
-		return $http.post('http://maximumstock.net/schwarmlernen/api/v1/infos/'+uuid+'/ratings',msg)
+		return $http.post(url+'/infos/'+uuid+'/ratings',msg)
 	}
 	
 	this.postRatingOfRating=  function(msg,uuid){
-		return $http.post('http://maximumstock.net/schwarmlernen/api/v1/ratings/'+uuid+'/ratings',msg)
+		return $http.post(url+'/ratings/'+uuid+'/ratings',msg)
 	}
 	
 	this.postConfig=  function(uuid,msg){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/targets/'+uuid+'/config',msg)
+		return $http.put(url+'/targets/'+uuid+'/config',msg)
 	}
 	
 	this.postGlobalConfig=  function(uuid,msg){
-		return $http.put('http://maximumstock.net/schwarmlernen/api/v1/targets/'+uuid+'/globalconfig',msg)
+		return $http.put(url+'/targets/'+uuid+'/globalconfig',msg)
 	}
 	
 	this.getGlobalConfig=  function(uuid){
-		return $http.get('http://maximumstock.net/schwarmlernen/api/v1/targets/'+uuid+'/globalconfig')
+		return $http.get(url+'/targets/'+uuid+'/globalconfig')
 	}
 
 	this.getNodesByRef=  function(link){
@@ -122,15 +123,15 @@ app.service('Api', ['$http','$cookies', function($http,$cookies) {
 	
 	
 	this.postTarget= function(msg,uuid){ 
-		return $http.post("http://maximumstock.net/schwarmlernen/api/v1/targets/"+uuid+"/targets",msg)
+		return $http.post(url+"/targets/"+uuid+"/targets",msg)
   	}
   	
   	this.postMainTarget= function(msg){ 
-		return $http.post("http://maximumstock.net/schwarmlernen/api/v1/targets",msg)
+		return $http.post(url+"/targets",msg)
   	}
   	
   	this.getSelf= function(){ 
-		return $http.get("http://maximumstock.net/schwarmlernen/api/v1/self")
+		return $http.get(url+"/self")
 		.then(function(response) {
 			return response
   		},function(response) {
@@ -140,36 +141,36 @@ app.service('Api', ['$http','$cookies', function($http,$cookies) {
   	
   	
   	this.postTask= function(msg,uuid){ 
-		return $http.post("http://maximumstock.net/schwarmlernen/api/v1/targets/"+uuid+"/tasks",msg)
+		return $http.post(url+"/targets/"+uuid+"/tasks",msg)
   	}
   	
   	this.postInfo= function(msg,uuid){
-		return $http.post("http://maximumstock.net/schwarmlernen/api/v1/targets/"+uuid+"/infos",msg)
+		return $http.post(url+"/targets/"+uuid+"/infos",msg)
   	}
   	
   	this.postSolution= function(msg,uuid){
-		return $http.post("http://maximumstock.net/schwarmlernen/api/v1/tasks/"+uuid+"/solutions",msg)
+		return $http.post(url+"/tasks/"+uuid+"/solutions",msg)
   	}
   	
 	this.createUser= function(msg,uuid){ 
-		return $http.put("http://maximumstock.net/schwarmlernen/api/v1/targets/"+uuid+"/users",msg)
+		return $http.put(url+"/targets/"+uuid+"/users",msg)
   	}
   	
   	this.deleteTask= function(uuid){ 
-		return $http.put("http://maximumstock.net/schwarmlernen/api/v1/tasks/"+uuid+"/status")
+		return $http.put(url+"/tasks/"+uuid+"/status")
   	}
   	
   	this.deleteSolution= function(uuid){ 
-		return $http.put("http://maximumstock.net/schwarmlernen/api/v1/solutions/"+uuid+"/status")
+		return $http.put(url+"/solutions/"+uuid+"/status")
   	}
   	
   	this.deleteInfo= function(uuid){ 
-		return $http.put("http://maximumstock.net/schwarmlernen/api/v1/infos/"+uuid+"/status")
+		return $http.put(url+"/infos/"+uuid+"/status")
   	}
   	
   	//Saves Hash of User in Cookies
   	this.saveUserToken= function(msg){ 
-		return $http.post('http://maximumstock.net/schwarmlernen/api/v1/login',msg)
+		return $http.post(url+'/login',msg)
 		.then(function(response) {
 			console.log(response.data);
 			$cookies.put('X-Access-Token',response.data.token);
