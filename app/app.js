56f76f2230e28b057a17c35bb87ba599b8aa90ec
Bene <admin@Benes-Mac-Book-Pro.local>
2015-10-13 14:21:32 +0200

    abc

--- app/app.js
+++ app/app.js
@@ -3,95 +3,95 @@ var app = angular.module('slApp', ['ngRoute','ngCookies']);
 //This configures the routes and associates each route with a view and a controller
 app.config(['$routeProvider',function ($routeProvider) {
     $routeProvider
-        .when('#/Startseite',
+        .when('/Startseite',
             {
                 controller: 'StartseiteController',
-                templateUrl: 'schwarmlernen/app/partials/Startseite.html'
+                templateUrl: 'app/partials/Startseite.html'
             })
-        .when('#/Lernziele',
+        .when('/Lernziele',
             {
                 controller: 'LernzielController',
-                templateUrl: 'schwarmlernen/app/partials/Lernziele.html'
+                templateUrl: 'app/partials/Lernziele.html'
             })
-        .when('#/Konto',
+        .when('/Konto',
             {
                 controller: 'KontoController',
-                templateUrl: 'schwarmlernen/app/partials/Konto.html'
+                templateUrl: 'app/partials/Konto.html'
             })
-        .when('#/Abmelden',
+        .when('/Abmelden',
             {
                 controller: 'AbmeldenController',
-                templateUrl: 'schwarmlernen/app/partials/Abmelden.html'
+                templateUrl: 'app/partials/Abmelden.html'
             })
-        .when('#/addToTask:uuid*',
+        .when('/addToTask:uuid*',
             {
                 controller: 'AddToTargetController',
-                templateUrl: 'schwarmlernen/app/partials/AddToTask.html'
+                templateUrl: 'app/partials/AddToTask.html'
             })
-        .when('#/addToTarget:uuid*',
+        .when('/addToTarget:uuid*',
             {
                 controller: 'AddToTargetController',
-                templateUrl: 'schwarmlernen/app/partials/AddToTarget.html'
+                templateUrl: 'app/partials/AddToTarget.html'
             })
-        .when('#/addToDegree:uuid*',
+        .when('/addToDegree:uuid*',
             {
                 controller: 'AddModulController',
-                templateUrl: 'schwarmlernen/app/partials/AddModul.html'
+                templateUrl: 'app/partials/AddModul.html'
             })
-        .when('#/createUserFor:uuid*',
+        .when('/createUserFor:uuid*',
             {
                 controller: 'AddUserController',
-                templateUrl: 'schwarmlernen/app/partials/AddUser.html'
+                templateUrl: 'app/partials/AddUser.html'
             })
-        .when('#/User',
+        .when('/User',
             {
                 controller: 'UserController',
-                templateUrl: 'schwarmlernen/app/partials/User.html'
+                templateUrl: 'app/partials/User.html'
             })
-        .when('#/Login',
+        .when('/Login',
             {
                 controller: 'AnmeldenController',
-                templateUrl: 'schwarmlernen/app/partials/Anmelden.html'
+                templateUrl: 'app/partials/Anmelden.html'
             })
-       	.when('#/task:uuid*',
+       	.when('/task:uuid*',
             {
                 controller: 'TaskViewController',
-                templateUrl: 'schwarmlernen/app/partials/TaskView.html'
+                templateUrl: 'app/partials/TaskView.html'
             })
-        .when('#/info:uuid*',
+        .when('/info:uuid*',
             {
                 controller: 'InfoViewController',
-                templateUrl: 'schwarmlernen/app/partials/InfoView.html'
+                templateUrl: 'app/partials/InfoView.html'
             })
-        .when('#/solution:uuid*',
+        .when('/solution:uuid*',
             {
                 controller: 'SolutionViewController',
-                templateUrl: 'schwarmlernen/app/partials/SolutionView.html'
+                templateUrl: 'app/partials/SolutionView.html'
             })
-        .when('#/unfinishedinfo:uuid*',
+        .when('/unfinishedinfo:uuid*',
             {
                 controller: 'InfoUnfinishedController',
-                templateUrl: 'schwarmlernen/app/partials/InfoUnfinished.html'
+                templateUrl: 'app/partials/InfoUnfinished.html'
             })
-        .when('#/unfinishedsolution:uuid*',
+        .when('/unfinishedsolution:uuid*',
             {
                 controller: 'SolutionUnfinishedController',
-                templateUrl: 'schwarmlernen/app/partials/SolutionUnfinished.html'
+                templateUrl: 'app/partials/SolutionUnfinished.html'
             })
-        .when('#/unfinishedtask:uuid*',
+        .when('/unfinishedtask:uuid*',
             {
                 controller: 'TaskUnfinishedController',
-                templateUrl: 'schwarmlernen/app/partials/TaskUnfinished.html'
+                templateUrl: 'app/partials/TaskUnfinished.html'
             })
-        .when('#/config:uuid*',
+        .when('/config:uuid*',
             {
                 controller: 'ConfigController',
-                templateUrl: 'schwarmlernen/app/partials/Config.html'
+                templateUrl: 'app/partials/Config.html'
             })
-        .when('#/Admin',
+        .when('/Admin',
             {
                 controller: 'AdminPanelController',
-                templateUrl: 'schwarmlernen/app/partials/AdminPanel.html'
+                templateUrl: 'app/partials/AdminPanel.html'
             })
-        .otherwise({ redirectTo: 'schwarmlernen/app/partials/Startseite.html' });
+        .otherwise({ redirectTo: 'app/partials/Startseite.html' });
 }]);
