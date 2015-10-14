9d052a89b8e0e0eb764791b3ff6023c7445bf604
Bene <admin@Benes-Mac-Book-Pro.local>
2015-10-14 11:53:53 +0200

    lastLogin

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -21,7 +21,7 @@ app.controller('UserController',['$scope','Api','$routeParams','$cookies', funct
 			.then(function (user){
 				jQuery.each(user.data,function () {
 					console.log(this)
-					var lastLogin = moment(this.properties.lastLogin, "DDMMYYYY").fromNow();
+					var lastLogin = moment(this.properties.lastLogin, "YYYYMMDD").fromNow();
 					console.log(lastLogin);
 					//User darstellen
 					$('#ul'+parentID).append("<li style='background-color:grey' class='list-group-item id='"+this.properties.uuid+"' ><span class='form-control'>"+this.properties.username+"</span></li>");
